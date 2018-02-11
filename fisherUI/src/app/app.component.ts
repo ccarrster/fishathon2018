3aa0ea456115fb27739d1eeb8383bce2529725bc
Alex Mackinnon <alex@alexmackinnon.ca>
2018-02-10 20:12:10 -0500

    inital UI

---
+++ fisherUI/src/app/app.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  public index = 0;
+  public geopoints: Array<any> = [
+    {
+      index: 0,
+      name: 'Mary',
+      lat: -80.4984,
+      long: 43.4513,
+    },
+    {
+      index: 1,
+      name: 'Lee',
+      lat: -80.4984,
+      long: 33,
+    },
+    {
+      index: 2,
+      name: 'Joe',
+      lat: 80.4984,
+      long: 33
+    },
+  ];
+
+  public geopoint: any = {
+    lat: 0,
+    long: 0
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngOnInit() {
+    this.getData(geopoint);
+  }
+
+  updatePoint(person) {
+    console.log('person %s', person);
+    if (person === 'Mary') {
+      this.index = 0;
+    }
+    else if (person === 'Lee') {
+      this.index = 1;
+    }
+    else if (person === 'Joe') {
+      this.index = 2;
+    }
+    else {
+      this.index = 0;
+    }
+
+
+  }
+
+  getData(value) {
+    //['Browser', 'Operating Systems', 'Language', 'Cities'];
+    value = value.replace(/ /, '');
+    let url = this.baseherf + value + 'Data';
+    this.http.get(url)
+      .subscribe(
+        (data: any[]) => {
+          this.filterData(data);
+        }
+      );
+  }
+
+  filterData(data) {
+
+  }
+
+}
---
+++ fisherUI/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgxMapboxGLModule}  from 'ngx-mapbox-gl';
+
+import { AppComponent } from './app.component';
+
+
+
+/*
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleG1hY2tpbm5vbiIsImEiOiJjamRodHpybnkxMmQwMndwN3dqbm5hOWFvIn0.fvoLHQpY-Eqzx6V851jirQ';
+var map = new mapboxgl.Map({
+container: 'YOUR_CONTAINER_ELEMENT_ID',
+style: 'mapbox://styles/mapbox/streets-v10'
+});
+ */
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxMapboxGLModule.forRoot({
+          accessToken:
+          'pk.eyJ1IjoiYWxleG1hY2tpbm5vbiIsImEiOiJjamRodHpybnkxMmQwMndwN3dqbm5hOWFvIn0.fvoLHQpY-Eqzx6V851jirQ'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
